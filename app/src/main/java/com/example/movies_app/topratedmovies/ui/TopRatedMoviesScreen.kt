7be093c134a36312e27ddc37580eaa30646e9146
sye-13 <shenghao.ye.13+git@gmail.com>
2025-02-24 16:32:05 +0100

    Improve top rated movies UI screen

--- app/src/main/java/com/example/movies_app/topratedmovies/ui/TopRatedMoviesScreen.kt
+++ app/src/main/java/com/example/movies_app/topratedmovies/ui/TopRatedMoviesScreen.kt
@@ -1,26 +1,39 @@
 package com.example.movies_app.topratedmovies.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 
 @Composable
 fun TopRatedMoviesScreen(state: TopRatedMoviesState, onItemClick: (String) -> Unit) {
     when (state) {
-        is TopRatedMoviesState.Content -> Content(state.movies, onItemClick)
+        is TopRatedMoviesState.Content -> Content(
+            state.movies,
+            onItemClick
+        )
+
         TopRatedMoviesState.Error -> Text(
             text = "Error",
             modifier = Modifier
@@ -44,25 +57,50 @@ private fun Content(
 ) {
     Column {
         TopAppBar(title = { Text(text = "Movies") })
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            items(
-                items = topMovies,
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+        ) {
+            items(items = topMovies,
                 key = { it.id }
-            ) { topMovie ->
-                Card(modifier = Modifier.clearAndSetSemantics {
-                    contentDescription = topMovie.title
-                    onClick("show the movie details") {
-                        onItemClick(topMovie.id)
-                        true
-                    }
-                }, onClick = { onItemClick(topMovie.id) }) {
-                    AsyncImage(
-                        model = topMovie.posterPath,
-                        contentDescription = null,
-                    )
-                    Text(text = topMovie.title)
+            ) { movie ->
+                MovieItem(movie, onItemClick)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieItem(
+    movie: MovieUi,
+    onItemClick: (String) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .clearAndSetSemantics {
+                contentDescription = movie.title
+                onClick("show the movie details") {
+                    onItemClick(movie.id)
+                    true
                 }
             }
+            .fillMaxWidth()
+            .padding(12.dp),
+        shape = RoundedCornerShape(8.dp),
+        onClick = { onItemClick(movie.id) }) {
+        Row(
+            modifier = Modifier.padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = movie.posterPath,
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
